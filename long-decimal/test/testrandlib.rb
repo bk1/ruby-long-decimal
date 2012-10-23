5ed6d6e298fd95f2358e50d974e15ead6c0f6b8b
bk1 <bk1@gmx.net>
2012-10-23 17:48:20 +0200

    test/testrandlib.rb test/testrandpower.rb added

---
+++ long-decimal/test/testrandlib.rb
@@ -0,0 +1,103 @@
+#!/usr/bin/env ruby
+#
+# testrandlib.rb -- runit test for long-decimal.rb
+#
+# CVS-ID:    $Header: /var/cvs/long-decimal/long-decimal/test/testrandlib.rb,v 1.7 2006/05/07 20:00:24 bk1 Exp $
+# CVS-Label: $Name: ALPHA_01_02 $
+# Author:    $Author: bk1 $ (Karl Brodowsky)
+#
+
+require "crypt/ISAAC"
+
+#
+# test class for LongDecimal and LongDecimalQuot
+#
+module TestRandomHelper
+
+  @RCS_ID='-$Id: testrandlib.rb,v 1.7 2006/05/07 20:00:24 bk1 Exp $-'
+
+  @@r1 = Crypt::ISAAC.new
+  @@r2 = Crypt::ISAAC.new
+  @@r3 = Crypt::ISAAC.new
+  @@r4 = Crypt::ISAAC.new
+
+  def arr_with_sq(arr)
+    result = arr.map do |x|
+      if (x >= 0) then
+        r = LongMath.sqrt(x, x.scale, LongMath::ROUND_HALF_UP)
+        [ r, x, r*x, x.square ]
+      else
+        x
+      end
+    end
+    result.flatten
+  end
+
+  def arr_with_neg(arr)
+    arr.map { |x| [x, -x] }.flatten
+  end
+
+  def arr_with_inv(arr)
+    result = arr.map do |x|
+      if (x.zero? || x.one?) then
+        x
+      else
+        [x, x.inverse.to_ld(x.scale*2)]
+      end
+    end
+    result.flatten
+  end
+
+  #
+  # get random numbers in an array
+  # xb is between 0 and 1000
+  # xm is between 1 and 2
+  # xs is between 0 and 1
+  # all three are LongDecimal and have the same digits after the decimal point
+  # out of these an array of up to 36 values is formed and returned as a[0]
+  # are found by adding sqrt and square of of these to the array, if >= 0
+  # adding the reciprocal of each non-zero entry of this array to array
+  # and adding negated value of each entry to the array, then sorting
+  # array and removing duplicates
+  # a[1] is an integer between 0 and 120 used as precision for exp
+  # a[2] is an integer between 1 and 121 used as precision for log
+  # a[3] is an integer between 0 and 120+(x.scale)/2 used as precision for sqrt
+  # a[4] is an integer between 0 and 60 used as precision for power
+  #
+  def random_arr
+    x0 = @@r1.rand(1000)
+    x1 = @@r2.rand(1000)
+    x2 = @@r2.rand(100)+3
+    x3 = @@r3.rand(1000)
+    x4 = @@r3.rand(100)+4
+    x5 = @@r4.rand(1000)
+    x6 = @@r4.rand(100)+5
+    xs = LongDecimal(x1, x2) + LongDecimal(x3, x4) + LongDecimal(x5, x6)
+    xt = (xs / LongMath.pi(xs.scale)).to_ld(xs.scale*2)
+    xu = 1 + xs
+    xv = 1 + xt
+    xw = 2 + xs
+    xe = LongMath.exp(1, xs.scale) + xs
+    xp = LongMath.pi(xs.scale) + xs
+    xn = 10 + xs
+    xb = x0 + xs
+    xg = LongMath::MAX_EXP_ABLE - xs
+    xh = LongMath::MAX_EXP_ABLE + xs
+    eprec = @@r1.rand(120)
+    lprec = eprec+1
+    sprec = eprec+((xb.scale+1)>>1)
+    pprec = lprec >> 1
+    arr1 = [ xt, xs, xu, xv, xw, xe, xp, xn, xb, xg, xh ]
+#     f    = LongDecimal("0.25")
+#     arr2 = (0..8).map do |j|
+#       ff = f*j
+#       [ ff + xs, ff**5 + xs ]
+#     end
+#     arr3 = [ arr1, arr2 ].flatten
+    arr  = arr_with_neg(arr_with_inv(arr_with_sq(arr1))).sort.uniq
+    return [ arr, eprec, lprec, sprec, pprec, xb.scale ]
+  end
+
+end
+
+# end of file testrandlib.rb
---
+++ long-decimal/test/testrandpower.rb
@@ -0,0 +1,74 @@
+#!/usr/bin/env ruby
+#
+# testrandpower.rb -- runit test for long-decimal.rb
+#
+# CVS-ID:    $Header: /var/cvs/long-decimal/long-decimal/test/testrandpower.rb,v 1.9 2006/05/11 20:57:22 bk1 Exp $
+# CVS-Label: $Name: ALPHA_01_02 $
+# Author:    $Author: bk1 $ (Karl Brodowsky)
+#
+
+require "runit/testcase"
+require "runit/cui/testrunner"
+require "runit/testsuite"
+require "crypt/ISAAC"
+
+load "test/testlongdeclib.rb"
+load "test/testrandlib.rb"
+
+#
+# test class for LongDecimal and LongDecimalQuot
+#
+class TestRandomPower_class < RUNIT::TestCase
+  include TestLongDecHelper
+  include TestRandomHelper
+
+  @RCS_ID='-$Id: testrandpower.rb,v 1.9 2006/05/11 20:57:22 bk1 Exp $-'
+
+  # for how many seconds should this test run? change to different
+  # value on demand
+  @@duration = 1000000
+
+  #
+  # test the calculation of the eyponential function
+  #
+  def test_random_power
+    cnt  = 0
+    scnt = 0
+    t0   = Time.new
+    while (true) do
+      t1 = Time.new
+      d  = t1 - t0
+      break if d >= @@duration
+      xarr, eprec1, lprec1, sprec1, pprec1, xs = random_arr
+      yarr, eprec2, lprec2, sprec2, pprec2, ys = random_arr
+
+      prec = (pprec1+pprec2) >> 1
+      xarr.each do |x|
+        puts(" t=#{Time.new-t1} x=#{x}: ")
+        next if x <= 0
+        # next if (x < 1.1)
+        # next if (x < 1)
+        # next if (x < 0.99)
+	next if x.abs > LongMath::MAX_FLOATABLE || (1/x).abs > LongMath::MAX_FLOATABLE
+        yarr.each do |y|
+          puts("\ncnt=#{cnt} scnt=#{scnt} x=#{x} y=#{y} scx=#{x.scale} scy=#{y.scale} prec=#{prec}")
+          # next if x < 0.001 && y < 0
+          next if x > 1 && y > LongMath::MAX_EXP_ABLE * 2
+          next if x < 1 && y < -LongMath::MAX_EXP_ABLE * 2
+          next if Math.log(1+x.to_i) * y.to_i > LongMath::MAX_EXP_ABLE
+          next if -Math.log(1+(1/x).to_i) * y.to_i > LongMath::MAX_EXP_ABLE
+          next if (Math.log(x.to_f) * y.to_f).abs > LongMath::MAX_EXP_ABLE * 2
+          scnt += 1
+          check_power_floated(x, y, prec)
+        end
+      end
+      cnt += 1
+    end
+    puts("done #{cnt} tests\n")
+  end
+
+end
+
+RUNIT::CUI::TestRunner.run(TestRandomPower_class.suite)
+
+# end of file testrandpower.rb
